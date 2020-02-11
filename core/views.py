a908c2272cf514d1a4bdf538e8a2d8e316a86c34
unknown <papajuju123@naver.com>
2020-02-11 21:44:56 +0900

    2020_02_12

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import os
+import requests, json
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import FormView, DetailView, UpdateView
+from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from . import models
+from django.contrib.messages.views import SuccessMessageMixin
+import urllib.request
 
-# Create your views here.
+
+def firstindecide(request):
+    user = request.user
+    print(user.is_authenticated)
+
+    return render(request, "users/login.html")
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,11 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.conf.urls.static import static
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("users.urls", namespace="users")),
+    path("", include("core.urls", namespace="core")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

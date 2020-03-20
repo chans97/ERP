010c777204026829fbec1f74cf4a9e1eac67f46f
unknown <papajuju123@naver.com>
2020-03-20 23:19:58 +0900

    2020_03_18_35

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,16 +16,9 @@ import sys
 from django.core.wsgi import get_wsgi_application
 from django.core.handlers.wsgi import WSGIHandler
 
-
+sys.path.append('/opt/bitnami/apps/django/django_projects/cirucs')
 os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
-try:
-    application = get_wsgi_application()
-except Exception:
-    # Error loading applications
-    if "mod_wsgi" in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
+application = get_wsgi_application()
 
 # 파이썬의 절대경로
 # sys.path.append("C:/Apache24/htdocs/<PROJECT-ROOT-DIRECTORY>")

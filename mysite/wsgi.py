777f47db446ad59cb1eab94eb51fe1d453101119
unknown <papajuju123@naver.com>
2020-05-12 13:19:21 +0900

    2020_05_04_1

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,25 +1,16 @@
 """
-WSGI config for mysite project.
+WSGI config for ebdjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-
 import os
-import time
-import traceback
-import signal
-import sys
+
 from django.core.wsgi import get_wsgi_application
-from django.core.handlers.wsgi import WSGIHandler
 
-sys.path.append('/opt/bitnami/apps/django/django_projects/cirucs')
-os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 application = get_wsgi_application()
-
-# 파이썬의 절대경로
-# sys.path.append("C:/Apache24/htdocs/<PROJECT-ROOT-DIRECTORY>")
-
